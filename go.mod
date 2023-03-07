a1621d86a788fa1f648ba50b1aeef4fb35d230ed
mariasouza <mariasouza@brasilcard.net>
2023-03-07 11:41:18 -0300

    feat- go-api

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mariaeduardasouza/golang-api
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/mariaeduardasouza/golang-api/routes"
+)
+
+func main() {
+	routes.HandleRequest()
+}
